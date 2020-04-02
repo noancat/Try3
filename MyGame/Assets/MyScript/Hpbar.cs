b6e3b5ab9eace75621a76ed81723ccde536baed9
noancat <62477446+noancat@users.noreply.github.com>
2020-04-02 21:43:29 +0400

    косметические изменения

---
+++ MyGame/Assets/MyScript/Hpbar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Hpbar : MonoBehaviour
+{
+  // Start is called before the first frame update
+    public Image bar;
+    public static float fill;
+    public static bool smert;
+    void Start()
+    {
+        fill = 1f;
+        smert = false;
+
+    }
+    void Update()
+    {
+        bar.fillAmount = fill;
+        if (fill <=0)
+        {
+            smert = true;
+        }
+    }
+}
