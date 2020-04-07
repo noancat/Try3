277ecac714d5bba345ab39c70a07bf0549054668
noancat <62477446+noancat@users.noreply.github.com>
2020-04-07 13:08:58 +0400

    добавил

---
+++ MyGame/Assets/myScript/AccessCard.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AccessCard : PolimorfScript
+{
+    public static bool coinPikced;
+    public GameObject card;
+
+    public override void Use()
+    {
+        coinPikced = true;
+        card.SetActive(true);
+        base.Use();
+        card.SetActive(true);
+    }
+    public new void Start()
+    {
+        coinPikced = false;
+        card.SetActive(false);
+        GameManager.GetInstance().cardPicked += Use;
+    }
+    private void FixedUpdate()
+    {
+        if (coinPikced)
+        {
+            GameManager.GetInstance().CardIsGet();
+            Destroy(gameObject);
+        }
+    }
+}
+
