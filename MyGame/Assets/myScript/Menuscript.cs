937177ee658d914a862ccc9428b6f51a2b404817
noancat <62477446+noancat@users.noreply.github.com>
2020-04-07 11:52:25 +0400

    убрал папку libry

---
+++ MyGame/Assets/myScript/Menuscript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menuscript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public virtual void Tomenu()
+    {
+        GameManager.GetInstance().LoadScene(0);
+    }
+    public void StartGame()
+    {
+        GameManager.GetInstance().LoadScene(1);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
